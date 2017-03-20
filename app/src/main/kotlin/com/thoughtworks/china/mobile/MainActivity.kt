bdf8c551b18f62c08d1a8a162c9dce963c64fa06
Zhang Shuai <520zhangshuai@gmail.com>
2017-03-20 17:59:24 +0800

    Add main activity.kt

---
+++ app/src/main/kotlin/com/thoughtworks/china/mobile/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.thoughtworks.china.mobile
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import mobile.china.thoughtworks.com.java_vs_kotlin.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        val fab = findViewById(R.id.fab) as FloatingActionButton
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+}
