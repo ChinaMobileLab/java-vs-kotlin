b5f20e254950a0bbe49de5b417be796494f7f8f4
Zhiyong Wang <zywang@zywang.local>
2017-03-20 17:50:27 +0800

    Added sealed class demo.

---
+++ app/src/main/kotlin/com/thoughtworks/china/mobile/SealedClassDemo.kt
@@ -0,0 +1,26 @@
+package com.thoughtworks.china.mobile
+
+sealed class Expr
+
+data class Const(val number: Int) : Expr()
+data class Add(val e1: Expr, val e2: Expr) : Expr()
+data class Minus(val e1: Expr, val e2: Expr) : Expr()
+data class Multiple(val e1: Expr, val e2: Expr) : Expr()
+data class Divide(val e1: Expr, val e2: Expr) : Expr()
+
+fun Expr.eval(): Const =
+        when (this) {
+            is Const -> this
+            is Add -> Const(e1.eval().number + e2.eval().number)
+            is Minus -> Const(e1.eval().number - e2.eval().number)
+            is Multiple -> Const(e1.eval().number * e2.eval().number)
+            is Divide -> Const(e1.eval().number / e2.eval().number)
+        }
+
+
+object Main {
+    @JvmStatic fun main(args: Array<String>) {
+        // 5+4-3*2/1
+        println("${Minus(Add(Const(5), Const(4)), Divide(Multiple(Const(3), Const(2)), Const(1))).eval()}")
+    }
+}
