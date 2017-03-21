c569b9f767d770ae66f9869d12200764ec11e789
Zhiyong Wang <zywang@zywang.local>
2017-03-21 09:18:17 +0800

    Added inline function demo.

---
+++ app/src/main/kotlin/com/thoughtworks/china/mobile/inlinefunction/InlinedFunctionDemo.kt
@@ -0,0 +1,32 @@
+package com.thoughtworks.china.mobile.inlinefunction
+
+import com.google.gson.Gson
+import java.util.concurrent.locks.Lock
+import java.util.concurrent.locks.ReentrantLock
+
+object InlinedFunctionDemo {
+
+    inline fun <T> lock(lock: Lock, body: () -> T): T {
+        lock.lock()
+        val result = body()
+        lock.unlock()
+        return result
+    }
+    fun demoInline(): String {
+        val lock = ReentrantLock()
+        lock(lock) {
+            return "test"
+        }
+    }
+
+    inline fun <reified T> fromJson(json: String): T {
+        return Gson().fromJson(json, T::class.java)
+    }
+    fun demoReified() = fromJson<String>("test")
+
+    inline var str: String
+        get() {
+            return ""
+        }
+        set(v) {}
+}
